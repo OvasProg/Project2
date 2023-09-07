0951bb16c6987163dfde2060923fc78ca6480982
OvasProg <alex.vasyliev.prog@gmail.com>
2023-09-07 13:54:53 +0300

    1

--- src/Part2/part2.java
+++ src/Part2/part2.java
@@ -3,10 +3,9 @@ package Part2;
 import java.util.Scanner;
 
 public class part2 {
+    //create a new scanner
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-
-        //create a new scanner
-        Scanner in = new Scanner(System.in);
 
         //option for user to quit
         System.out.println("Type 'q' anytime to quit (except age, height and weight questions)");
@@ -14,51 +13,42 @@ public class part2 {
         while (true) {
 
             //ask user's name
-            System.out.println("What is your name?");
-            String name = in.nextLine();
+            String name = getUserStringValue("What is your name?");
             if (name.equals("q")) {
                 break;
             }
 
             //ask user's last name
-            System.out.println("What is your last name?");
-            String lastname = in.nextLine();
+            String lastname = getUserStringValue("What is your last name?");
             if (lastname.equals("q")) {
                 break;
             }
 
             //ask user's nickname
-            System.out.println("What is your nickname?");
-            String nickname = in.nextLine();
+            String nickname = getUserStringValue("What is your nickname?");
             if (nickname.equals("q")) {
                 break;
             }
 
             //ask user's country
-            System.out.println("Where are you from?");
-            String country = in.nextLine();
+            String country = getUserStringValue("Where are you from?");
             if (country.equals("q")) {
                 break;
             }
 
             //ask user's age
-            System.out.println("How old are you?");
-            int your_age = in.nextInt();
-            in.nextLine();
+            int your_age = getUserIntegerValue("How old are you?");
 
             //ask user's weight
-            System.out.println("How much do you weigh (kg) (use comma for fractions)");
-            double weight = in.nextDouble();
-            in.nextLine();
+            double weight = getUserDoubleValue("How much do you weigh (kg) (use comma for fractions)");
+
 
             //ask user's height
-            System.out.println("How tall are (m) (use comma for fractions)");
-            double height = in.nextDouble();
-            in.nextLine();
+            double height = getUserDoubleValue("How tall are (m) (use comma for fractions)");
+
 
             //ask user's hobby
-            System.out.println("What is your hobby?");
-            String hobby = in.nextLine();
+            String hobby = getUserStringValue("What is your hobby?");
             if (hobby.equals("q")) {
                 break;
             }
@@ -85,5 +75,23 @@ public class part2 {
 
         }
 
+    }
+
+    public static int getUserIntegerValue(String caption){
+        System.out.println(caption);
+        int x = in.nextInt();
+        in.nextLine();
+        return x;
+    }
+    public static double getUserDoubleValue(String caption){
+        System.out.println(caption);
+        double x = in.nextDouble();
+        in.nextLine();
+        return x;
+    }
+    public static String getUserStringValue(String caption){
+        System.out.println(caption);
+        String x = in.nextLine();
+        return x;
     }
 }
